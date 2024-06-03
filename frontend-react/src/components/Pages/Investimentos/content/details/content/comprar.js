7bf670f1c1f47a50825f0947ce708f8fd1d454b2
GuilhermeSilva3176 <guilhermecunha3176@gmail.com>
2024-06-03 20:20:37 -0300

    Alterações no comprar do investimentos

--- frontend-react/src/components/Pages/Investimentos/content/details/content/comprar.js
+++ frontend-react/src/components/Pages/Investimentos/content/details/content/comprar.js
@@ -5,18 +5,18 @@ function Comprar({ onClose }) {
   return (
     <div className="comprar-overlay active" onClick={onClose}>
       <div className="comprar-content active" onClick={(e) => e.stopPropagation()}>
-        <h2 className="comprarTitle">Comprar</h2>
         <button onClick={onClose} className="fecharAba"></button>
-        <form>
-          <div className="form-group">
+        <h2 className="comprarTitle">Comprar</h2>
+        <form className="formComprar">
+          <div className="formComprar-group">
             <label htmlFor="produto">Produto:</label>
             <input type="text" id="produto" />
           </div>
-          <div className="form-group">
+          <div className="formComprar-group">
             <label htmlFor="quantidade">Quantidade:</label>
             <input type="number" id="quantidade" />
           </div>
-          <div className="form-group">
+          <div className="formComprar-group">
             <label htmlFor="precoUnitario">Preço Unitário:</label>
             <input type="number" id="precoUnitario" />
           </div>

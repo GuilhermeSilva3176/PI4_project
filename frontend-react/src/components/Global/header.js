5f02ad6561596ec14b544cd0e82eac4558537cb3
GuilhermeSilva3176 <guilhermecunha3176@gmail.com>
2024-06-06 13:50:42 -0300

    feat: Adiciona no header um collapse
    - Adiciona collapse para o header quando estiver em paginas menores.
    - Consertta o css no HEADER e adiciona responsividade para o collapse.
    - Adciona animação ao clicar no collapse.
    
    desc: Este commit tem a intenção de arrumar o header e faze-lo ficar mais
          responsivo ao tamanho das paginas, alem disso adiciona uma animação
          que acontece ao clicar no botão de collapse.

--- frontend-react/src/components/Global/header.js
+++ frontend-react/src/components/Global/header.js
@@ -1,77 +1,101 @@
-import React from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import './header.css';
 import axios from "axios";
-import {TOKEN_EXPIRATION_REF, USER_TOKEN_REF} from "../../constants/constants";
+import { TOKEN_EXPIRATION_REF, USER_TOKEN_REF } from "../../constants/constants";
 
-function Header(){
-  let navigate = useNavigate()
+function Header() {
+  let navigate = useNavigate();
+  const location = useLocation();
   const isAuthenticated = localStorage.getItem(USER_TOKEN_REF) !== null &&
-      localStorage.getItem(TOKEN_EXPIRATION_REF) > Date.now();
+    localStorage.getItem(TOKEN_EXPIRATION_REF) > Date.now();
+
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
   const handleLogout = () => {
-    if (!window.confirm('Deseja realmente sair?')) return
+    if (!window.confirm('Deseja realmente sair?')) return;
 
-    axios.post('http://localhost:8080/api/v1/auth/logout', {
-        headers: {
-          'Authorization': 'Bearer ' + localStorage.getItem(USER_TOKEN_REF)
-        }
+    axios.post('http://localhost:8080/api/v1/auth/logout', {}, {
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem(USER_TOKEN_REF)
+      }
     }).then((response) => {
       if (response.status === 200) {
-        localStorage.removeItem('user-token')
-        localStorage.removeItem('token-expiry-date')
-        localStorage.removeItem('user-name')
+        localStorage.removeItem('user-token');
+        localStorage.removeItem('token-expiry-date');
+        localStorage.removeItem('user-name');
 
-        alert('Logout efetuado com sucesso')
+        alert('Logout efetuado com sucesso');
 
-        navigate("/")
+        navigate("/");
       }
     }).catch((error) => {
-      alert('Erro ao fazer logout ' + error)
-    })
-  }
+      alert('Erro ao fazer logout ' + error);
+    });
+  };
 
   const changeHomeRoute = () => {
-    if (isAuthenticated) {
-      return '/home'
-    } else {
-      return '/'
-    }
-  }
+    return isAuthenticated ? '/home' : '/';
+  };
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 992) {
+        setIsCollapsed(true);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <header id="headerChart">
       <div className="container-fluid">
-        <nav className='navbar navbar-expand-lg navbar-dark justify-content-between fixed-top'>
-
-          <Link className='navbar-logo' to={changeHomeRoute()}>
-            <img src='/images/header_logo.svg' alt='Header Logo' className='img-fluid'/>
+        <nav className="navbar navbar-expand-lg navbar-dark justify-content-between fixed-top">
+          <Link className="navbar-logo" to={changeHomeRoute()}>
+            <img src='/images/header_logo.svg' alt='Header Logo' className='img-fluid' />
           </Link>
-
-          <nav className="collapse navbar-collapse" id="navbarNav">
+          <button
+            className={`navbar-toggler ${isCollapsed ? 'collapsed' : 'expanded'}`}
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded={!isCollapsed}
+            aria-label="Toggle navigation"
+            onClick={() => setIsCollapsed(!isCollapsed)}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className={`collapse navbar-collapse ${isCollapsed ? '' : 'show'}`} id="navbarNav">
             <ul className="navbar-nav ms-auto mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link" to={changeHomeRoute()}>Home</Link>
+                <Link className={`nav-link ${location.pathname === '/home' ? 'active' : ''}`} to={changeHomeRoute()}>Home</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/financas">Finanças</Link>
+                <Link className={`nav-link ${location.pathname === '/financas' ? 'active' : ''}`} to="/financas">Finanças</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/investimentos">Carteira</Link>
+                <Link className={`nav-link ${location.pathname === '/investimentos' ? 'active' : ''}`} to="/investimentos">Carteira</Link>
+              </li>
+              <li>
+                <div className={`nav-line ${isCollapsed ? '' : 'd-none'}`}></div>
               </li>
               <li className="nav-item">
                 {isAuthenticated ?
                   <a className="nav-link" onClick={handleLogout}>Logout</a> :
-                  <Link className="nav-link" to="/login">Login</Link>
+                  <Link className={`nav-link ${location.pathname === '/login' ? 'active' : ''}`} to="/login">Login</Link>
                 }
               </li>
             </ul>
-          </nav>
+          </div>
         </nav>
       </div>
     </header>
-  )
+  );
 }
 
 export default Header;
